3411dc4d3361666bfe6cfe2148292959e7d670d7
Stanislavikus <sb.mysignin@gmail.com>
2025-10-07 18:00:33 +0200

    Завдання 2
    Клонуйте FastAPI застосунок, налаштуйте і запустіть його в Docker контейнері. Перевірте правильність роботи застосунку та підключення до бази даних.
    
    
    
    Покрокова інструкція
    
    
    
    1. Використовуючи команду git clone, клонуйте репозиторій за адресою https://github.com/GoIT-Python-Web/Computer-Systems-hw02. Перейдіть у клонований каталог.
    
    
    
    2. Створіть Dockerfile із вказівками для створення образу Docker застосунку.
    
    
    
    3. Напишіть docker-compose.yaml з конфігурацією для застосунку та PostgreSQL.
    
    
    
    4. Використайте Docker Compose для побудови середовища, команду docker-compose up для запуску середовища.
    
    
    
    💡 Підказка: 
    Внесіть зміни в рядку підключення до бази даних SQLALCHEMY_DATABASE_URL: вона знаходиться у файлі \\conf\\db.py. Замість localhost вставте ім'я сервісу PostgreSQL з вашого файлу docker-compose.yaml .
    
    SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://postgres:567234@localhost:5432/hw02"
    
    Коли ви використовуєте Docker Compose, кожен сервіс (контейнер) має власну мережу, і вони зазвичай не можуть звертатися один до одного за допомогою localhost. Замість цього, ви маєте використовувати назву сервісу в якості імені хоста.
    
    
    
    5. Перевірте функціональність застосунку та доступність бази даних.
    
    
    
    💡 Підказка:
    
    
    Після запуску контейнера із застосунком вигляд у браузері повинен бути наступним:
    
    
    
    
    
    
    Якщо все правильно налаштовано у файлі docker-compose.yaml, натиснувши кнопку Перевірити БД, ви повинні побачити наступне:
    
    
    
    
    
    
    Якщо замість"Welcome to FastAPI!” ви бачите червоне віконце з повідомленням про помилку, значить ви неправильно налаштували docker-compose.yaml

---
+++ Computer-Systems-hw02/conf/db.py
@@ -0,0 +1,19 @@
+# conf/db.py
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://postgres:567234@db:5432/hw02"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
